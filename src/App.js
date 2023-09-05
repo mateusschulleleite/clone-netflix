868d3107c9777b8f796310fe98a3945896f71a88
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2023-09-05 08:12:00 -0300

    componente popular criado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import HighlightedMovie from "./components/HighlightedMovie";
+import PopularOnNetflix from "./components/PopularOnNetflix";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HighlightedMovie />
+      <PopularOnNetflix />
     </div>
   );
 }
